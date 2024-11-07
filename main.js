f99b1dc23fe72e9ddeaace098b00e37a295e9b2a
Federico Nova <fedenova8@gmail.com>
2024-11-07 12:11:20 -0300

    Comenzamos a estilizar desde estilos-retro.css

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './estilos.css'
+import './estilos-retro.css'
 
 /* 
 * import './estilos.css'
 * import './estilos-retro.css'
-* import './estilos.urbano.css'
+* import './estilos.futuro.css'
 */
